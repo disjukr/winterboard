914bc370b01ff7e07e0d9f7a86f5db138aa689cb
JongChan Choi <disjukr@naver.com>
2014-02-17 20:34:32 +0900

    on off

--- winterboard.js
+++ winterboard.js
@@ -16,16 +16,20 @@
     var croquis = new Croquis();
     var croquisDOMElement = croquis.getDOMElement();
     $(container).append(croquisDOMElement);
-    $(croquisDOMElement).bind('mousedown', function (e) {
+    $(croquisDOMElement).on('mousedown', down);
+    function down(e) {
       croquis.down(e.clientX, e.clientY);
-      $(document).bind('mousemove', function (e) {
-        croquis.move(e.clientX, e.clientY);
-      });
-      $(document).bind('mouseup', function (e) {
-        croquis.up(e.clientX, e.clientY);
-        $(document).unbind('mousemove mouseup');
-      });
-    });
+      $(document).on('mousemove', move)
+                 .on('mouseup', up);
+    }
+    function move(e) {
+      croquis.move(e.clientX, e.clientY);
+    }
+    function up(e) {
+      croquis.up(e.clientX, e.clientY);
+      $(document).off('mousemove', move)
+                 .off('mouseup', up);
+    }
     croquis.setCanvasSize(400, 300);
     croquis.addLayer();
     croquis.fillLayer('#fff');
