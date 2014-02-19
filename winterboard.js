92fe054973404907dadc95afc7669ce42ad66da4
JongChan Choi <disjukr@naver.com>
2014-02-19 10:57:06 +0900

    i love iframe

--- winterboard.js
+++ winterboard.js
@@ -15,20 +15,32 @@
     var self = this;
     var croquis = new Croquis();
     var croquisDOMElement = croquis.getDOMElement();
-    $(container).append(croquisDOMElement);
-    $(croquisDOMElement).on('mousedown', down);
+    var viewport = $('<iframe sandbox="allow-scripts allow-same-origin" frameborder="0">');
+    var viewportDocument;
+    viewport.ready(function () {
+      viewportDocument = viewport.contents()[0];
+      var viewportBody = viewportDocument.body;
+      $(viewportBody).append(croquisDOMElement);
+      $(viewportBody).css('margin', '0');
+      $(viewportBody).css('overflow', 'hidden');
+      $(viewportBody).css('background-color', '#333');
+      $(viewportDocument).on('mousedown', down);
+    });
+    $(container).append(viewport);
+    $(viewport).css('width', '800px');
+    $(viewport).css('height', '600px');
     function down(e) {
       croquis.down(e.clientX, e.clientY);
-      $(document).on('mousemove', move)
-                 .on('mouseup', up);
+      $(viewportDocument).on('mousemove', move)
+                         .on('mouseup', up);
     }
     function move(e) {
       croquis.move(e.clientX, e.clientY);
     }
     function up(e) {
       croquis.up(e.clientX, e.clientY);
-      $(document).off('mousemove', move)
-                 .off('mouseup', up);
+      $(viewportDocument).off('mousemove', move)
+                         .off('mouseup', up);
     }
     croquis.setCanvasSize(400, 300);
     croquis.addLayer();
